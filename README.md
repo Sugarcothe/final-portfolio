18e636f702af7e46ec38944a6446bad2f724fe7a
Sugarcothe <63057848+Sugarcothe@users.noreply.github.com>
2021-07-31 01:35:35 +0100

    origin main

---
+++ README.md
@@ -0,0 +1,29 @@
+### description
+
+A simple Portfolio template for developer/designers built with React. 
+
+### [live preview](https://ubai97.github.io/react-portfolio/)
+
+![react portfoiio](src/assets/images/react-porfolio.png)
+
+### Setup
+
+Get the code.
+
+ `git clone https://github.com/ubai97/react-portfolio.git`
+ 
+Install required dependencies
+
+`yarn install`
+
+modify pages content in  `src/content_option.js`.
+
+statrt the server
+
+`yarn start`
+
+### Thanks
+If you like this portfolio template don't forget give it a ⭐ 
+ 
+
+
---
+++ src/content_option.js
@@ -0,0 +1,184 @@
+const logotext = "iCodeTill.xyz";
+const meta = {
+    title: "iCodeTill.xyz",
+    description: "My name is Valentine, i enjoy writing code and making stuff work on the web. i have good experiences, and building on them, i learn fast and always open to learn more from anywhere and anybody",
+};
+
+const introdata = {
+    title: "I’m Valentine",
+    animated: {
+        first: "I write Frontend",
+        second: "I code Backend",
+        third: "Learning DevOps and Cloud",
+    },
+    description: "My name is Valentine, i enjoy writing code and making stuff work on the web. i have good experiences, and building on them, i learn fast and always open to learn more from anywhere and anybody",
+    
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "A bit about myself",
+    aboutme: "I have seen myself learning javascript and frameworks fast, this have made my programming journey fun; React, Sass, Bulma Css, Tailwind, Bootstrap, Redux, integrating Node packages and intrestingly backend of Express and NestJS. I have built awesome web applications and participated in software architecture and also databases entity relational diagram (ERD) development. Currently learning DevOps. I write clean and readable codes and while at that, I am dedicated to a life of learning new things everyday.",
+};
+
+const worktimeline = [{
+        jobtitle: "Responsive web design",
+        where: "Freecode Camp",
+        date: "2020",
+        link: 'https://www.freecodecamp.org/certification/fccfc062c2b-fe45-43d0-bb2d-58474b4be5cc/responsive-web-design'
+    },
+    {
+        jobtitle: "Web Frameworks",
+        where: "Freecodecamp",
+        date: "2020",
+        link: 'https://www.freecodecamp.org/certification/fccfc062c2b-fe45-43d0-bb2d-58474b4be5cc/front-end-libraries'
+    },
+    {
+        jobtitle: "React",
+        where: "Hackerrank",
+        date: "2021",
+        link: 'https://www.hackerrank.com/certificates/4b4dc06904a5'
+    },
+    {
+        jobtitle: "Javascript",
+        where: "Hackerrank",
+        date: "2021",
+        link: 'https://www.hackerrank.com/certificates/e8e45b8c57a2'
+    },
+];
+
+const worktimeline2 = [{
+        jobtitle: "Frontend Developer",
+        where: "HNG Internship",
+        date: "2018",
+    },
+    {
+        jobtitle: "Frontend/Frontend Tutor",
+        where: "Yobetech",
+        date: "2019",
+    },
+    {
+        jobtitle: "Fullstack Developer",
+        where: "Yammieshoppers",
+        date: "2021",
+    },
+];
+
+const skills = [{
+        name: "Javascript",
+        value: 90,
+    },
+    {
+        name: "NestJs",
+        value: 85,
+    },
+    {
+        name: "Express",
+        value: 80,
+    },
+    {
+        name: "Dependecies",
+        value: 90,
+    },
+    {
+        name: "Styling",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "Frontend",
+        description: "Building mind bugging UI/UX is always my watchward, i stick to design or make changes when client wants. I make use of Bootstrap, tailwind css, SASS, and Bulma CSS, while making it functional, calling the backend services, scaleable, and functional",
+    },
+    {
+        title: "Backend",
+        description: "Skilled with node frameworks of expressJs and nestJs. Performing their CRUD operation/Endpoints, integration to database, and deployment",
+    },
+    {
+        title: "Working on Codebase",
+        description: "At leisure time i study my code, open source codes, try fixing some legacy codes and learn DevOps",
+    },
+    {
+        title: "Googling",
+        description: "I would honestly accept that as a self-taught developer, it has always been google. As much as i know my syntax and algorithm well, i am good at being able to find out new things, find solutions to challenges, or just checking up random stuffs and how they work. I enjoy and know the skill well. I find everything in google ;-)",
+    },
+];
+
+const dataportfolio = [
+    {
+        img: "https://source.unsplash.com/user/benji3pr/icrhAD-qidc/400x400",
+        desctiption: "Always current wildfire tracker. Data fetched from NASA Database for developement, map is got from google map",
+        link: "https://sugarcothe.github.io/wild-fire-tracker",
+    },
+    {
+        img: "https://source.unsplash.com/user/flowforfrank/1seONCyPWfQ/400x400",
+        desctiption: "A realtime Markup Coverter",
+        link: "https://codepen.io/Sugarcothe/pen/wvJYYgd",
+    },
+    {
+        img: "https://source.unsplash.com/user/pappigo/OJ4m4_yIgjY/400x400",
+        desctiption: "Beat Box",
+        link: "https://codepen.io/Sugarcothe/full/mdWQJBE",
+    },
+    {
+        img: "https://source.unsplash.com/user/linkendinsalesnavigator/1LyBcHrH4J8/400x400",
+        desctiption: "Realtime video chat Application, built on Node using web3tc for peer to peer video chat",
+        link: "https://cocky-hypatia-0ee9d8.netlify.app",
+    },
+    {
+        img: "https://source.unsplash.com/user/markusspiske/wL7pwimB78Q/400x400",
+        desctiption: "An Ecommerce Backend Built on nestJs",
+        link: "https://github.com/Sugarcothe/nest-commerce-app",
+    },
+    {
+        img: "https://source.unsplash.com/user/anete_lusina/zwsHjakE_iI/400x400",
+        desctiption: "A Blog built in React/Express",
+        link: "https://sugarcothe.github.io/bloga-burger",
+    },
+    {
+        img: "https://source.unsplash.com/user/tobolenkoph/jSKMsSoiEtU/400x400",
+        desctiption: "A chat application using socket.io",
+        link: "https://pidgin-boxx.herokuapp.com",
+    },
+    {
+        img: "https://source.unsplash.com/user/rupixen/Q59HmzK38eQ/400x400",
+        desctiption: "An Ecommerce site built on basic Javascript and and backend on Node",
+        link: "https://www.yammieshoppers.com",
+    },
+
+    {
+        img: "https://source.unsplash.com/user/amoltyagi2/UZFCQPNc4bs/400x400",
+        desctiption: "A realtime calculator",
+        link: "https://codepen.io/Sugarcothe/full/LYWXJRe",
+    },
+    
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "ifeanyivalentine82@gmail.com",
+    YOUR_FONE: "+2347067869822",
+    description: "Let's talk right?!",
+    YOUR_SERVICE_ID: "service_lj95wfq",
+    YOUR_TEMPLATE_ID: "template_whzqabk",
+    YOUR_USER_ID: "user_CPUrZMkDdKmWI19xzxeJA",
+};
+
+const socialprofils = {
+    github: "https://github.com/Sugarcothe",
+    facebook: "#",
+    linkedin: "https://www.linkedin.com/in/valentineeze/",
+    twitter: "https://twitter.com/sweetestshuga",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    worktimeline2,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
