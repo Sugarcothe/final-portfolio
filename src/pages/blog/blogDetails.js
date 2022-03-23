f3419fbf9ee90919d7f7423ca44bbfcc0da42d0f
Sugarcothe <ifeanyivalentine82@gmail.com>
2022-03-23 08:29:43 +0000

    details done

--- src/pages/blog/blogDetails.js
+++ src/pages/blog/blogDetails.js
@@ -1,18 +1,19 @@
 import React from "react";
+import './blogDetails.css'
 
 
   
 const blogDetails = () => {
   return (
-    <div className="home">
+    <div className="blog-details">
      <div className='header-section'>
-      <h1>HEADER TITLE</h1>
-      <h4>Header title and about few lines of tech and thats all we should have here</h4>
-      <p>Date is today</p>
+      <h1 className="head-1">HEADER TITLE</h1>
+      <h4 className="head-2">Header title and about few lines of tech and thats all we should have here</h4>
+      <p className="date-1">Date is today</p>
      </div>
 
-     <div className='body'>
-      <p>Occaecat anim irure eiusmod officia non occaecat. Aute anim sint nostrud Lorem laboris. Dolor pariatur nisi exercitation ut in labore sit officia. Minim minim anim aliqua nostrud. Commodo dolore laboris irure aliqua reprehenderit dolore. Incididunt velit culpa labore ad elit. Eu esse esse ea ipsum consectetur qui elit. Veniam nisi mollit labore magna. Laboris dolor deserunt dolore amet duis veniam in ex. Deserunt deserunt in aute quis. Irure voluptate sunt Lorem labore. Incididunt pariatur ipsum dolor pariatur ea minim.</p>
+     <div className='details'>
+      <p className='details-1'>Occaecat anim irure eiusmod officia non occaecat. Aute anim sint nostrud Lorem laboris. Dolor pariatur nisi exercitation ut in labore sit officia. Minim minim anim aliqua nostrud. Commodo dolore laboris irure aliqua reprehenderit dolore. Incididunt velit culpa labore ad elit. Eu esse esse ea ipsum consectetur qui elit. Veniam nisi mollit labore magna. Laboris dolor deserunt dolore amet duis veniam in ex. Deserunt deserunt in aute quis. Irure voluptate sunt Lorem labore. Incididunt pariatur ipsum dolor pariatur ea minim.</p>
      </div>
     </div>
   );
